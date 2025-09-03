598c3cafdfc928f2fdba24033d21db0a8406ba2d
baptistev10-ops <baptiste.dwwm@gmail.com>
2025-09-03 16:49:25 +0200

    Header interactif avec la connexion et la déconnexion de l'utilisateur

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,12 @@
 import { NavLink } from "react-router-dom";
 import logosmall from "../assets/images/Bap_1024x1024.png";
 import { FiUser } from "react-icons/fi";
+import { useAuth } from "./context/AuthContext";
 
 export default function Header() {
+  const { userConnected } = useAuth();
+  const { logout } = useAuth();
+
   return (
     <header className="backdrop-blur-sm bg-white bg-opacity-70 max-h-[100px] flex justify-center items-center border-b border-gray-300 sticky top-0 w-screen">
       <NavLink to="/" className="h-full w-[25vw] ml-10">
@@ -14,26 +18,56 @@ export default function Header() {
         />
       </NavLink>
 
-      <nav className="flex gap-4 font-[Inter] font- w-[50vw] justify-center">
-        <NavLink
-          to="/vendre"
-          className="hover:bg-slate-200 transition ease-in duration-100 rounded-lg py-1 px-3"
-        >
-          Vendre
-        </NavLink>
-        <NavLink
-          to="/annonces"
-          className="hover:bg-slate-200 transition ease-in duration-100 rounded-lg py-1 px-3"
-        >
-          Mes annonces
-        </NavLink>
-        <NavLink
-          to="/login"
-          className="hover:bg-slate-200 transition ease-in duration-100 rounded-lg py-1 px-3 flex gap-1 items-center"
-        >
-          <FiUser /> Connexion
-        </NavLink>
-      </nav>
+      {userConnected ? (
+        <nav className="flex gap-4 font-[Inter] font- w-[50vw] justify-center">
+          <NavLink
+            to="/vendre"
+            className="hover:bg-slate-200 transition ease-in duration-100 rounded-lg py-1 px-3"
+          >
+            Vendre
+          </NavLink>
+          <NavLink
+            to="/annonces"
+            className="hover:bg-slate-200 transition ease-in duration-100 rounded-lg py-1 px-3"
+          >
+            Mes annonces
+          </NavLink>
+          <NavLink
+            to="/"
+            className="hover:bg-slate-200 transition ease-in duration-100 rounded-lg py-1 px-3 flex gap-1 items-center"
+          >
+            <FiUser /> Profil
+          </NavLink>
+          <NavLink
+            to="/"
+            className="hover:bg-slate-200 transition ease-in duration-100 rounded-lg py-1 px-3 flex gap-1 items-center"
+            onClick={logout}
+          >
+            Déconnexion
+          </NavLink>
+        </nav>
+      ) : (
+        <nav className="flex gap-4 font-[Inter] font- w-[50vw] justify-center">
+          <NavLink
+            to="/vendre"
+            className="hover:bg-slate-200 transition ease-in duration-100 rounded-lg py-1 px-3"
+          >
+            Vendre
+          </NavLink>
+          <NavLink
+            to="/annonces"
+            className="hover:bg-slate-200 transition ease-in duration-100 rounded-lg py-1 px-3"
+          >
+            Mes annonces
+          </NavLink>
+          <NavLink
+            to="/login"
+            className="hover:bg-slate-200 transition ease-in duration-100 rounded-lg py-1 px-3 flex gap-1 items-center"
+          >
+            <FiUser /> Connexion
+          </NavLink>
+        </nav>
+      )}
 
       <div className="w-[25vw] mr-10">
         <button className="bg-gray-950 text-white py-2 px-4 rounded-lg">
