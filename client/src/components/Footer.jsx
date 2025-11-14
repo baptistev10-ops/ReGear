1d9192584eaac00432ea89e1c15549e73c0e7677
baptistev10-ops <baptiste.dwwm@gmail.com>
2025-11-14 20:35:13 +0100

    added mentions légales

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,13 +1,15 @@
+import { NavLink } from "react-router-dom";
+
 export default function Footer() {
   return (
     <footer className="w-full bg-gray-100 py-6 mt-12 border-t">
       <div className="max-w-6xl mx-auto px-6 flex justify-center">
-        <a
+        <NavLink
           href="/mentions-legales"
           className="text-sm text-gray-600 hover:text-black transition"
         >
           Mentions légales
-        </a>
+        </NavLink>
       </div>
     </footer>
   );
